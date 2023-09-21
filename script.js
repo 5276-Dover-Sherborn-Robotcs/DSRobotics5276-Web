9b81b95dcb9a3fce10aedbc6bb35fb93fc5694da
William Goldman <60369048+iliketocode2@users.noreply.github.com>
2023-09-20 22:23:29 -0400

    more team page updates

--- script.js
+++ script.js
@@ -12,58 +12,24 @@ function responsive() {
   }
 }
 
-// //code to open popup (old code we can remove later)
-// var isPopOpen = false;
-// var first = true;
-// var otherElement = false;
-// var y;
-// var popup;
+// reveal team images as you scroll
+function reveal() {
+  var reveals = document.querySelectorAll(".reveal");
 
-// function openPopup(x){
-//   //you've clicked on a popup somewhere
-//   //if a popup is already open
-//   if (isPopOpen){
-//     //hide old popup
-//     popup = document.getElementById(y);
-//     popup.classList.toggle("show");
-//     //show new popup
-//     popup = document.getElementById(x);
-//     popup.classList.toggle("show");
-//     y = x; 
-//     otherElement = true;
-//   }
-//   //otherwise show the popup
-//   else {
-//     y = x; 
-//     popup = document.getElementById(x);
-//     popup.classList.toggle("show");
-//     isPopOpen = true;
-//   }
-// }
-// //if i click anywhere...
-// window.addEventListener('click', function(e){ 
-//   if (isPopOpen){
-//     //if this is the first time im clicking on an element, dont do anything
-//     if (first){
-//       first = false;
-//     }
-//     else{
-//       //if i click outside of the open popup...
-//       if (!popup.contains(e.target)) {
-//         //if no element is currently open
-//         if (!otherElement){
-//           document.getElementById(y).classList.remove("show");
-//           isPopOpen = false;
-//           first = true;
-//         }
-//         //if another element is already open, dont hide the current popup
-//         else{
-//           otherElement = false;
-//         }
-//       }
-//     }
-//   }
-// });
+  for (var i = 0; i < reveals.length; i++) {
+    var windowHeight = window.innerHeight;
+    var elementTop = reveals[i].getBoundingClientRect().top;
+    var elementVisible = 150;
+
+    if (elementTop < windowHeight - elementVisible) {
+      reveals[i].classList.add("active");
+    } else {
+      reveals[i].classList.remove("active");
+    }
+  }
+}
+
+window.addEventListener("scroll", reveal);
 
 //change between alumni and students tab
 var i, tabcontent, tablinks;
