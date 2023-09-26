e44ffdfd4ca53fff8087aba0d869e200fa036d67
William Goldman <60369048+iliketocode2@users.noreply.github.com>
2023-09-26 15:28:34 -0400

    Revert "*i don't know what to write"
    This reverts commit bd6725a14ed1923894fd0181b95e656988f56bb8.

--- script.js
+++ script.js
@@ -44,18 +44,12 @@ function openTab(load, evt, tab) {
     tablinks[i].className = tablinks[i].className.replace(" active", "");
   }
   if (load == 1) {
-<<<<<<< Updated upstream
     document.getElementById("center").className += " active";
     //center the members tab
     document.getElementById("slide").left += width / 2;
   }
   document.getElementById(tab).style.display = "block";
   //apply active trait for new css properties
-=======
-    document.getElementById("init").className += " active";
-  }
-  document.getElementById(tab).style.display = "block";
->>>>>>> Stashed changes
   if (evt.currentTarget.classList.contains("active")) {
     evt.currentTarget.className -= " active";
   } else {
@@ -63,7 +57,6 @@ function openTab(load, evt, tab) {
   }
 }
 
-<<<<<<< Updated upstream
 // control animation for centering the horizontal tab buttons
 const l = document.getElementById("left");
 const r = document.getElementById("right");
@@ -119,14 +112,9 @@ function right() {
     add("slideLeft");
   }
 }
-=======
-// // Slideshow on covid years page
->>>>>>> Stashed changes
 
-// let slideIndex = 1;
-// showSlides(slideIndex);
+// Slideshow on covid years page
 
-<<<<<<< Updated upstream
 // let slideIndex = 1;
 // showSlides(slideIndex);
 
@@ -156,39 +144,6 @@ function right() {
 //   captionText.innerHTML = dots[slideIndex-1].alt;
 // }
 
-=======
-// function plusSlides(n) {
-//   showSlides((slideIndex += n));
-// }
-
-// function currentSlide(n) {
-//   showSlides((slideIndex = n));
-// }
-
-// function showSlides(n) {
-//   let i;
-//   let slides = document.getElementsByClassName("mySlides");
-//   let dots = document.getElementsByClassName("demo");
-//   let captionText = document.getElementById("caption");
-//   if (n > slides.length) {
-//     slideIndex = 1;
-//   }
-//   if (n < 1) {
-//     slideIndex = slides.length;
-//   }
-//   for (i = 0; i < slides.length; i++) {
-//     slides[i].style.display = "none";
-//   }
-//   for (i = 0; i < dots.length; i++) {
-//     dots[i].className = dots[i].className.replace(" active", "");
-//   }
-//   slides[slideIndex - 1].style.display = "block";
-//   dots[slideIndex - 1].className += " active";
-//   captionText.innerHTML = dots[slideIndex - 1].alt;
-// }
-
-//////
->>>>>>> Stashed changes
 ///////////////////////////////////////
 // Slider
 const slider = function () {
